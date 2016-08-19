71d2f4808ec82574c16c4268dbd3c28c51b31942
JustinChou <justinchou186@gmail.com>
2016-08-19 12:09:58 +0800

    初始化项目

---
+++ JustinMarquee/JustinMarquee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JustinMarquee
+//
+//  Created by JustinChou on 16/8/19.
+//  Copyright © 2016年 JustinChou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JustinMarquee/JustinMarquee/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JustinMarquee
+//
+//  Created by JustinChou on 16/8/19.
+//  Copyright © 2016年 JustinChou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
